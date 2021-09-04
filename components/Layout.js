e83b1d9f341b7ab09ec81b40a2b4d8afce4f27bc
kaito <sea_rr10_ch@outlook.com>
2021-09-04 17:40:52 +0900

    style:prettier設定

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,6 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head';
+import Link from 'next/link';
 
-export default function Layout({ children, title= "HP bu Nextjs"}) {
-    return(
-        <div></div>
-    )
+export default function Layout({ children, title = 'HP bu Nextjs' }) {
+  return <div className="flex justify-center items-center flex-col min-h-screen text-gray-600 text-sm font-mono"></div>;
 }
--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Blog = () => {
-    return (
-        <div>Blog page</div>
-    )
-}
+  return <div>Blog page</div>;
+};
 
-export default Blog
+export default Blog;
